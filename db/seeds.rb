0e4c4e451586aa47020f1ab084ffca64267f9127
Thomas Klein <thomas.klein@coupa.com>
2018-07-25 16:59:38 +0200

    Integrated index action for Portfolio page

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@
 
 9.times { |i|
   Portfolio.create!(
-     title: "Portfolio title: #{}",
+     title: "Portfolio title: #{i}",
      subtitle: "My great service",
      body: "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).",
      main_image: "http://via.placeholder.com/350x200",
