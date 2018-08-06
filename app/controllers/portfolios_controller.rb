5d40d74ad775f5f93236becc9f92df42e9ebabe2
Thomas Klein <thomas.klein@coupa.com>
2018-08-06 15:24:17 +0200

    Updating a portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,9 +2,13 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def new
     @portfolio = Portfolio.new
+  end
+
+  def edit
+    @portfolio = Portfolio.find(params[:id])
   end
 
   def create
@@ -16,6 +20,20 @@ class PortfoliosController < ApplicationController
         format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    @portfolio = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio }
+      else
+        format.html { render :edit }
         format.json { render json: @portfolio.errors, status: :unprocessable_entity }
       end
     end
