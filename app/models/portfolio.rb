03e0b07ffba462ed1cf8c83e33f5d5ec3d57063d
Thomas Klein <thomas.klein@coupa.com>
2018-08-07 12:25:35 +0200

    Implemented concern for genrating image placeholder

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,14 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
   validates_presence_of :title, :body, :main_image, :thumb_image
-  
+
   scope :ruby_on_rails_portfol_items, -> { where(subtitle: "Ruby on Rails") }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= generate_image(350, 200)
+    self.thumb_image ||= generate_image(60, 60)
+  end
 end
