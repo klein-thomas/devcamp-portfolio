5e543d28cc65046bf164773f9a9029a63da62890
Thomas Klein <thomas.klein@coupa.com>
2018-08-10 15:57:00 +0200

    New helper for the source tracking block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
       render 'app/guest_user_menu'
     end
   end
+
+  def source_helper(layout_name)
+    if session[:source]
+      content_tag :p, "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} page.", class: "source-greeting"
+    end
+  end
 end
