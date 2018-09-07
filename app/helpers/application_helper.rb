f62face1f9d43bf55b56f69aed9dcab8d9b8d5ac
Thomas Klein <thomas.klein@coupa.com>
2018-09-07 13:54:42 +0200

    Generified and unified the navigation management

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,29 @@ module ApplicationHelper
 
   def source_helper(layout_name)
     if session[:source]
-      content_tag :p, "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} page.", class: "source-greeting"
+      content_tag :p, "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} page.", class: 'source-greeting'
     end
+  end
+
+  def nav_helper
+    nav_items
+      .map { |it| yield it[:title], it[:url], { active: active?(it[:url]) } }
+      .join
+      .html_safe
+    nil
+  end
+
+  def nav_items
+    [
+      { title: 'Home', url: root_path },
+      { title: 'About Me', url: about_path },
+      { title: 'Blogs', url: blogs_path },
+      { title: 'Portfolios', url: portfolios_path },
+      { title: 'Contact', url: contact_path }
+    ]
+  end
+
+  def active?(path)
+    current_page? path
   end
 end
